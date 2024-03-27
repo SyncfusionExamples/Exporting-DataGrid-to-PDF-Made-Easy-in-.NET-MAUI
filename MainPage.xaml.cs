2ab926b8354af99a0d1d364773393b43fa173a8d
Farjana Parveen Ayubb <farjanaparveena@syncfusion.com>
2024-03-27 12:31:40 +0530

    Sample added

---
+++ MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Maui.Controls.PlatformConfiguration;
+using Syncfusion.Maui.DataGrid;
+using Syncfusion.Maui.DataGrid.Exporting;
+using System.Collections.ObjectModel;
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+      
+        private void ExportToPdf_Clicked(object sender, EventArgs e)
+        {
+            MemoryStream stream = new MemoryStream();
+            DataGridPdfExportingController pdfExport = new DataGridPdfExportingController();
+            DataGridPdfExportingOption option = new DataGridPdfExportingOption();
+            //var list = new List<string>();
+            //list.Add("OrderID");
+            //list.Add("CustomerID");
+            // option.ExcludedColumns = list;
+            //option.CanFitAllColumnsInOnePage = true;
+            //option.CanExportHeader = false;
+            //option.GridLineType = GridLineType.Horizontal;
+            //option.CanApplyGridStyle = true;
+            var pdfDoc = pdfExport.ExportToPdf(this.dataGrid, option);
+            pdfDoc.Save(stream);
+            pdfDoc.Close(true);
+            SaveService saveService = new();
+            saveService.SaveAndView("Export Feature.pdf", "application/pdf", stream);
+        }
+    }
+}
---
+++ Services/SaveService.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace MauiApp1
+{
+    public partial class SaveService
+    {
+
+        //Method to save document as a file and view the saved document.
+        public partial void SaveAndView(string filename, string contentType, MemoryStream stream);
+    }
+}
